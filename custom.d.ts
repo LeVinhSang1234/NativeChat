0ba1316f2014dca2682732298aec70283b4519b8
SangLv <sanglv@sskpi.com>
2022-05-05 09:55:16 +0700

    remove package json

--- custom.d.ts
+++ custom.d.ts
@@ -10,6 +10,5 @@ declare module 'react-native-vector-icons/Feather';
 declare module 'react-native-vector-icons/Ionicons';
 declare module 'react-native-vector-icons/EvilIcons';
 declare module 'rn-photos-framework';
-declare module 'react-native-i18n';
 declare module 'react-native-video';
 declare module 'react-native-convert-ph-asset';
